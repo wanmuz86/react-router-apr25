dfd20391602549db701ba7cb07394f81cf6699a6
Wan Muzaffar <wanmuz86@gmail.com>
2025-04-16 09:33:51 +0700

    add router using createBrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,31 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
+import Home from './components/Home.jsx'
+import About from './components/About.jsx'
+import Contact from './components/Contact.jsx'
+
+const routes = createBrowserRouter([
+  // Defining the route and which components to render
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/about',
+    element:<About/>
+  },
+  {
+    path: '/contact',
+    element:<Contact/>
+  }
+])
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={routes}/>
   </StrictMode>,
 )
