39d9bccfc11ab77f37b80ba0144d1088f8b82cc0
Wan Muzaffar <wanmuz86@gmail.com>
2025-04-16 11:03:58 +0700

    add useNavigate example

--- src/pages/ProductError.jsx
+++ src/pages/ProductError.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { useRouteError } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
 
 const ProductError = () => {
     const error = useRouteError();
+    const navigate = useNavigate();
   return (
     <div style={{padding:'16px'}}>
         <h2>Product Error</h2>
         <p>This is error for Product page : {error.statusText || error.message}</p>
-        <button>Browser List of product</button>
+        <button onClick={()=>navigate('/products')}>Browser List of product</button>
 
     </div>
   )
